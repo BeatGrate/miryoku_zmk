72adfe9219d39deb7397c644aa2c8d326ccc59fb
BeatGrate <gregorycbondoc@gmail.com>
2025-10-18 23:38:47 +0000

    The last one passed

--- miryoku/mapping/39/vault_katana.h
+++ miryoku/mapping/39/vault_katana.h
@@ -14,7 +14,7 @@ N30, N31, K32, K33, K34,      K35, K36, K37, N38, N39 \
 K00  K01  K02  K03  K04  XXX  K05  K06  K07  K08  K09 \
 K10  K11  K12  K13  K14  XXX  K15  K16  K17  K18  K19 \
 K20  K21  K22  K23  K24  XXX  K25  K26  K27  K28  K29 \
-XXX          K33    K34       K36    K35          XXX
+XXX          K34    K33       K36    K35          XXX
 
 #endif
 
